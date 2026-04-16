bd37ba62496d1969411637b400a635e6d44638ba
brandonparker1369 <loopsforsky@gmail.com>
2026-04-16 12:18:48 -0400

    correcting the string

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -6,7 +6,7 @@ public class WhileLoop {
         int count = 1;
         while (count <= 5)
         {
-            System.out.println("Welcome to Java!");
+            System.out.println("I Love Java!");
             count++;
         }
 
